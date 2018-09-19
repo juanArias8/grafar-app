fd77cebbef9f70115a26a1cb07c8128fccdee8fa
juanArias8 <juasda96@gmail.com>
2018-09-19 00:24:26 -0500

    create 3d graphs values ready

--- controllers/controller_graph.py
+++ controllers/controller_graph.py
@@ -2,22 +2,48 @@ import sympy as sym
 import numpy as np
 
 
-class Graph:
+# https://plot.ly/javascript/line-charts/
+class Graph2D:
     def __init__(self, function_param, a_param, b_param):
         self.function_param = function_param
         self.a_param = a_param
         self.b_param = b_param
 
-        self.fx = sym.sympify(self.function_param)
         self.x = sym.Symbol('x')
+        self.fx = sym.sympify(self.function_param)
 
     @np.vectorize
     def evaluate_fx(self, x_value):
         return self.fx.subs(self.x, x_value)
 
-    def create_graph_points(self):
-        x = np.linspace(self.a_param, self.b_param, 1000)
-        y = self.evaluate_fx(self, x)
+    def create_points_graph_2d(self):
+        vector_x = np.arange(self.a_param, self.b_param, 0.05)
+        vector_y = self.evaluate_fx(self, vector_x)
+        return list(vector_x), list(vector_y)
 
-        return list(x), list(y)
+
+class Graph3D():
+    def __init__(self, function_param, a_param, b_param):
+        self.function_param = function_param
+        self.a_param = a_param
+        self.b_param = b_param
+
+        self.x = sym.Symbol('x')
+        self.y = sym.Symbol('y')
+        self.fxy = sym.sympify(self.function_param)
+
+        def evaluate_fxy(val_x, val_y):
+            return float(self.fxy.subs((self.x, val_x), (self.y, val_y)))
+
+        def create_points_graph_3d(self):
+            vector_x = vector_y = np.arange(self.a_param, self.b_param, 0.05)
+            vector_x, vector_y = np.meshgrid(vector_x, vector_y)
+
+            z = np.array(
+                [evaluate_fxy(i, j) for i, j in
+                 zip(np.ravel(vector_x), np.ravel(vector_y))]
+            )
+            z = z.reshape(vector_x.shape)
+
+            return list(vector_x), list(vector_y), list(z)
 
--- resources/client/client.py
+++ resources/client/client.py
@@ -1,9 +1,9 @@
-import json
+import os
 
 from flask import Flask
 from flask import jsonify
-from flask import make_response
 from flask import render_template
+from flask import make_response
 
 from flask_restful import Api
 from flask_restful import Resource
@@ -12,11 +12,16 @@ from resources.client.request import parser_function
 from resources.database.database import graphs_collection
 
 from controllers import controller_database
-from controllers.controller_graph import Graph
+from controllers.controller_graph import Graph2D
 from controllers.metadata import FunctionFields
 
+static_folder = './../../static'
+template_folder = './../../templates'
+kwargs = dict(template_folder=template_folder, static_folder=static_folder)
+
 # Create Flask instance
-app = Flask(__name__)
+app = Flask(__name__, **kwargs)
+
 api_client = Api(app)
 
 
@@ -33,12 +38,13 @@ class Function(Resource):
         message = 'Error'
         data_function = parser_function.parse_args()
         try:
-            graph = Graph(
+            graph = Graph2D(
                 data_function[FunctionFields.function],
                 data_function[FunctionFields.param_a],
                 data_function[FunctionFields.param_b]
             )
-            x, y = graph.create_graph_points()
+            x, y = graph.create_points_graph_2d()
+            x, y = str(x), str(y)
             controller_database.insert_graph(
                 graphs_collection, data_function[FunctionFields.function], x, y
             )
@@ -47,10 +53,7 @@ class Function(Resource):
             message = 'Error ' + str(e)
             success = False
         else:
-            message = json.dumps({
-                'x': str(x),
-                'y': str(y)
-            })
+            message = {'x': x, 'y': y}
             success = True
         finally:
             answer = jsonify({
