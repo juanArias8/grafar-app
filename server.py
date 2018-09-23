3fe7c27942599387b0bb4bb45ca5af2bdf7ec0e1
juanArias8 <juasda96@gmail.com>
2018-09-23 02:14:27 -0500

    Debug mode disabled

--- server.py
+++ server.py
@@ -16,5 +16,6 @@ __license__ = 'GPL'
 api_client.add_resource(Index, '/')
 api_client.add_resource(Function, '/function')
 
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1')
